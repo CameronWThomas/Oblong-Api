c0f55fcc87fa1d852f48e946403a180d86721fb1
Cameron Thomas <cthomas@chirontech.com>
2022-05-01 12:20:30 -0400

    Add project files.

---
+++ Oblong-Api/Controllers/AccessLoggingController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Oblong_Api.Models;
+using System.Net;
+
+namespace Oblong_Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccessLoggingController : ControllerBase
+    {
+        public static readonly string[] accessTypes = new[]
+        {
+            "enter", "exit"
+        };
+
+        [HttpPost(Name ="AccessSite")]
+        public string Post(int accessType, string? token = null)
+        {
+            HttpContext context = this.HttpContext;
+            //enter
+            if(accessType == 0)
+            {
+                //string ip = context.Connection.RemoteIpAddress;
+                IPAddress ip = context.Connection.RemoteIpAddress;
+                var x = 0;
+                SiteAccess access = new SiteAccess();
+                return "test-token";
+
+            }//exit
+            else if(accessType == 1)
+            {
+                return "test-token";
+            }
+            else
+            {
+                //?
+                return "test-token";
+            }
+        }
+    }
+}
---
+++ Oblong-Api/Models/SiteAccess.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Oblong_Api.Models
+{
+    [Table("site_access")]
+    public class SiteAccess
+    {
+        public int Id { get; set; }
+        public string? SiteName { get; set; }
+        public string? IP { get; set; }
+        public DateTime TimeEntered { get; set; }
+        public DateTime TimeExited { get; set; }
+    }
+}
